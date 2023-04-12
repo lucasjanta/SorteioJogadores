ddf3a7e576bd3f9c86dde7a17c43e5563ae56e92
lucasjanta <lucastyle2014@gmail.com>
2023-04-11 22:59:09 -0300

    Criação do código para adicionar jogadores

--- codigo.js
+++ codigo.js
@@ -1,19 +1,117 @@
-const formulario = document.querySelector('#formbagre');
-const lista = document.querySelector('.gridplayers');
+// Verifica se já existe um array de jogadores no localStorage
+let playersArray = JSON.parse(localStorage.getItem('playersArray')) || [];
 
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
+// Seleciona os elementos do DOM
+const nameInput = document.querySelector('#inputnome');
+const tierInput = document.querySelector('#inputtier');
+const form = document.querySelector('form');
+// Seleciona o elemento .gridplayers no DOM
+const gridPlayers = document.querySelector('.gridplayers');
 
-    const nomeImput = document.querySelector('#inputnome').value;
-    const tierbagre = document.querySelector('#inputtier').value;
+// Função para adicionar um novo jogador ao array
+function addPlayer(name, tier) {
+  const player = { nome: name, tier: tier };
+  playersArray.push(player);
+  localStorage.setItem('playersArray', JSON.stringify(playersArray));
+}
 
-    const jogador = {
-        nome: nomeImput,
-        tier: tierbagre
-    };
+function renderPlayers() {
+    // Remove todos os elementos de jogadores existentes
+    while (gridPlayers.firstChild) {
+      gridPlayers.removeChild(gridPlayers.firstChild);
+    }
+    
+    // Percorre o array de jogadores e cria um novo elemento de jogador para cada objeto no array
+    playersArray.forEach((player, index) => {
+      // Cria um novo elemento HTML para o jogador
+      const newPlayer = document.createElement('div');
+      newPlayer.classList.add('player');
+      
+      // Adiciona o nome do jogador
+      const playerName = document.createElement('h1');
+      playerName.textContent = player.nome;
+      newPlayer.appendChild(playerName);
+      
+      // Adiciona a pontuação do jogador
+      const playerScore = document.createElement('div');
+      playerScore.classList.add('pontuacao');
+      newPlayer.appendChild(playerScore);
+      
+      // Adiciona o checkbox do jogador
+      const playerCheckbox = document.createElement('input');
+      playerCheckbox.setAttribute('type', 'checkbox');
+      playerCheckbox.setAttribute('name', player.nome);
+      playerCheckbox.setAttribute('id', player.nome.toLowerCase());
+      newPlayer.appendChild(playerCheckbox);
+  
+      const playerEditButton = document.createElement('button');
+      playerEditButton.textContent = 'Editar';
+      playerEditButton.addEventListener('click', () => {
+        editPlayer(index);
+      });
+      newPlayer.appendChild(playerEditButton);
+  
+      const playerDeleteButton = document.createElement('button');
+      playerDeleteButton.textContent = 'Excluir';
+      playerDeleteButton.addEventListener('click', () => {
+        deletePlayer(index);
+      });
+      newPlayer.appendChild(playerDeleteButton);
+  
+      
+      // Adiciona o novo jogador à lista
+      gridPlayers.appendChild(newPlayer);
+    });
+  }
+  
+  // Chame a função renderPlayers() sempre que um novo jogador for adicionado
+  function addPlayer(name, tier) {
+    const player = { nome: name, tier: tier };
+    playersArray.push(player);
+    localStorage.setItem('playersArray', JSON.stringify(playersArray));
+    renderPlayers();
+  }
 
-    localStorage.setItem('jogador', JSON.stringify(jogador));
+// Adiciona um evento de submissão ao formulário
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // impede o envio do formulário
+  addPlayer(nameInput.value, tierInput.value);
+  nameInput.value = ''; // Limpa o input do nome
+  tierInput.value = ''; // Limpa o input do tier
+});
 
-    const jogadorLocal = JSON.parse(localStorage.getItem('jogador'));
-    console.log(jogadorLocal);
-})
+
+const editPlayer = index => {
+    // Pede ao usuário um novo nome e tier para o jogador
+    const newNome = prompt('Digite o novo nome do jogador:');
+    const newTier = prompt('Digite o novo tier do jogador:');
+  
+    // Atualiza o objeto jogador correspondente no array
+    playersArray[index].nome = newNome;
+    playersArray[index].tier = newTier;
+  
+    // Salva o array de jogadores atualizado no localstorage
+    localStorage.setItem('playersArray', JSON.stringify(playersArray));
+  
+    // Renderiza a lista de jogadores atualizada
+    renderPlayers();
+  }
+
+
+
+const deletePlayer = index => {
+    // Remove o objeto jogador correspondente do array
+    playersArray.splice(index, 1);
+  
+    // Salva o array de jogadores atualizado no localstorage
+    localStorage.setItem('playersArray', JSON.stringify(playersArray));
+  
+    // Renderiza a lista de jogadores atualizada
+    renderPlayers();
+  }
+
+
+
+
+
+// Função para renderizar a lista de jogadores
