b62097266ce0b938d30dbfcf6ebbd6e926a4b5eb
lucasjanta <lucastyle2014@gmail.com>
2023-04-26 01:46:21 -0300

    falta  tirar os jogadores do array inicial depois de sorteados

--- sorteio.js
+++ sorteio.js
@@ -1,6 +1,5 @@
 let playersArray = JSON.parse(localStorage.getItem('playersArray')) || [];
 const numeroJogadores = document.querySelector('#numerojogadores');
-const numeroTimes = document.querySelector('#numerotimes');
 const formsorteio = document.querySelector('#formsorteio');
 
 let arrayJogadores = [];
@@ -10,18 +9,34 @@ var lt1 = document.querySelector('.lt1');
 var lt2 = document.querySelector('.lt2');
 var lt3 = document.querySelector('.lt3');
 var lt4 = document.querySelector('.lt4');
-function sortearJogadores(numjogadores, numtimes) {
+function sortearJogadores(numjogadores) {
 
 console.log(arrayJogadores);
 
-for (let i = 0; i < arrayJogadores.length; i++) {
-  const player = arrayJogadores[i];
+var numtimes = Math.floor(arrayJogadores.length / numjogadores);
+console.log(numtimes);
+
+if (numtimes == 2) {
+  var times = [[],[]]
+  for(let i = 0; i < numtimes; i++) {
+    for (let u = 0; u < numjogadores; u++) {
+      for (let tierjogador = 3; tierjogador >= 1; tierjogador--){
+        var jogadoraleatorio = Math.floor(Math.random() * arrayJogadores.length);
+        while(arrayJogadores[jogadoraleatorio].tier != tierjogador){
+          jogadoraleatorio = Math.floor(Math.random() * arrayJogadores.length);//random de 0 a 18
+        }
+        times[i].push(arrayJogadores[jogadoraleatorio]);
+        console.log(times);
+    }
+    }
+  }
+}/*  else if (numtimes == 3) {
   
+} else if (numtimes == 4) {
   
-}
-
-
-
+} else if (numtimes == 5) {
+  
+} */
 
 
 
@@ -189,5 +204,5 @@ for (let i = 0; i < arrayJogadores.length; i++) {
   // Adiciona um evento de submissão ao formulário
 formsorteio.addEventListener('submit', function(event) {
   event.preventDefault(); // impede o envio do formulário
-  sortearJogadores(numeroJogadores.value, numeroTimes.value);
+  sortearJogadores(numeroJogadores.value);
 });
