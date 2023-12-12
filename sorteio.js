495abe6878be77e8e07f30558ca19e17e9d7129d
lucasjanta <lucastyle2014@gmail.com>
2023-12-12 20:12:50 -0300

    Atualizando o sorteio

--- sorteio.js
+++ sorteio.js
@@ -2,6 +2,7 @@ const burgermenu = document.querySelector('#burgermenu');
 const menushow = document.querySelector('.menu');
 let playersArray = JSON.parse(localStorage.getItem('playersArray')) || [];
 const numeroJogadores = document.querySelector('#numerojogadores');
+const numeroTimes = document.querySelector('#numerotimes');
 const formsorteio = document.querySelector('#formsorteio');
 
 var lt1 = document.querySelector('.lt1');
@@ -12,104 +13,33 @@ var lt4 = document.querySelector('.lt4');
 burgermenu.addEventListener('click', () => {
   menushow.classList.toggle('activestatus');
 })
-function sortearJogadores(numjogadores) {
-
-var arrayJogadores = [];
-arrayJogadores = playersArray;
+function sortearJogadores(numeroJogadores, numeroTimes) {
+  var arrayJogadores = [];
+  arrayJogadores = playersArray;
 //Define o número de times
-var numtimes = Math.floor(arrayJogadores.length / numjogadores);
-
-console.log(numtimes);
-//Cria os arrays para cada time
-switch (numtimes) {
-  case 1:
-    var times = [[]]
-    break;
-  case 2:
-    var times = [[], []]
-    break;
-  case 3:
-    var times = [[], [], []]
-    break;
-  case 4:
-    var times = [[], [], [], []]
-    break;
-  case 5:
-    var times = [[], [], [], [], []]
-    break;
-  case 6:
-    var times = [[], [], [], [], [], []]
-    break;
-}
-
-for(let i = 0; i < numtimes; i++) {
-  if(arrayJogadores.length == 0){
-    break;
+  var time = [];
+  for (let i = 0; i < numeroTimes; i++) {
+    time[i] = [];
+      for (let u = 0; u < numeroJogadores; u++) {
+        var randomPlayer = Math.floor(Math.random() * arrayJogadores.length);
+        time[i].push(arrayJogadores[randomPlayer]);
+        arrayJogadores.splice(randomPlayer, 1);
+      }
   }
-  let tierjogador = 3;
 
-
-  for (let u = 0; u < numjogadores; u++) {
-
-    let numtier = [0, 0, 0];
-
-    for (let i = 0; i < arrayJogadores.length; i++) {
-      if (arrayJogadores[i].tier == 3) {
-        numtier[2]++;
-      }
-    }
-
-    for (let i = 0; i < arrayJogadores.length; i++) {
-      if (arrayJogadores[i].tier == 2) {
-        numtier[1]++;
-      }
-    }
-
-    for (let i = 0; i < arrayJogadores.length; i++) {
-      if (arrayJogadores[i].tier == 1) {
-        numtier[0]++;
-      }
-    }
-
-      var jogadoraleatorio = Math.floor(Math.random() * arrayJogadores.length);
-
-      while(arrayJogadores[jogadoraleatorio].tier != tierjogador && numtier[tierjogador-1] > 0) {
-        jogadoraleatorio = Math.floor(Math.random() * arrayJogadores.length);//random de 0 a 18
-      }
-      if (numtier[tierjogador-1] == 0) {
-        tierjogador--;
-      } else {
-        times[i].push(arrayJogadores[jogadoraleatorio]);
-      arrayJogadores.splice(jogadoraleatorio, 1);
-      tierjogador--;
-      }
-      if(tierjogador == 0){
-        tierjogador = 3;
-      }
-      if(arrayJogadores.length == 0){
-        break;
-      }
-    }
+  console.log(time);
+  localStorage.setItem('times', JSON.stringify(time));
+  return time;
   }
+
+function atualizaLista() {
+  console.log("oi");
   
-  localStorage.setItem('times', JSON.stringify(times));
-  return times;
-  }
-
-function atualizaLista(timesSort) {
-  let listasTimes = document.querySelectorAll('.listaTime');
-  console.log(listasTimes.length);
-  
-  for (let i = 0; i < timesSort.length; i++) {
-    for (let u = 0; u < timesSort[i].length; u++) {
-      listasTimes[i].innerHTML += timesSort[i][u].nome + '<br>';
-    }
-  }
 }
 
   // Adiciona um evento de submissão ao formulário
 formsorteio.addEventListener('submit', function(event) {
   event.preventDefault(); // impede o envio do formulário
   /* var timessorteados = sortearJogadores(numeroJogadores.value); */
-  atualizaLista(sortearJogadores(numeroJogadores.value));
+  sortearJogadores(numeroJogadores.value, numeroTimes.value);
 });
